a3d30307e7c9df5cb4a3ea7051c481588451f54a
kato223272 <223272@ids.upchiapas.edu.mx>
2023-11-09 10:43:27 -0600

    crear cuenta e inicio acabado

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './App.css';
 import Footer from './Components/footer.jsx';
 import Navbar from './Components/Navbar.jsx';
 import Inicio from './Inicio.jsx';
-// import Crear from './Pages/CrearCuenta.jsx';
+// import Editar from './Pages/EditarPerfil.jsx';
 import InicioSesion from './Pages/IniciarSesion.jsx';
 // import Recuperar from './Pages/RecuperarContrasenia.jsx';
-// import Registrarse from './Pages/Registrarse.jsx';
+import Registrarse from './Pages/Registrarse.jsx';
 // import Seleccion from './Pages/Seleccion.jsx';
 
 
@@ -26,7 +26,7 @@ function App() {
         <Route path="/" element={<Inicio />} />
         {/* <Route path="/Crear" element={<Crear />} /> */}
         <Route path="/iniciosesion" element={<InicioSesion />} />
-        {/* <Route path="/registrarse" element={<Registrarse />} /> */}
+        <Route path="/registrarse" element={<Registrarse />} />
         {/* <Route path="/seleccion" element={<Seleccion />} /> */}
         {/* <Route path="/recuperarcontrasenia" element={<Recuperar />} /> */}
       </Routes>
